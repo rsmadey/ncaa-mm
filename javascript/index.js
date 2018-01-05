4f901702b6f784ece7559b4ecb693a7627fae2aa
root <root@ip-172-31-45-137.ec2.internal>
2018-01-05 04:20:49 +0000

    fix page jump

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,7 @@
 function determineLocation(){
 	var input = document.forms["phraseForm"]["keyphrase"].value;
 	if(input == 'mm2018'){
-		window.location.href="marchmadness/login.php";
+		return 'marchmadness/login.php';
 	}
+	return "index.html";
 }
